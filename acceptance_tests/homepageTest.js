2b3906f46dc0bcf2085cdd8b3244e681b78c8231
Asia Wolf <asia.nicole.wolf@gmail.com>
2016-06-30 12:33:17 -0400

    basic tests

--- acceptance_tests/homepageTest.js
+++ acceptance_tests/homepageTest.js
@@ -1,7 +1,15 @@
 casper.test.begin('a test to verify page title', 1, function(test) {
-    casper.start('http://localhost:3000/', function() {
-        test.assertExists('#name');
-    }).run(function() {
-        test.done();
-    });
+	casper.start('http://localhost:3000/', function() {
+		this.fillSelectors('#contactForm', {
+			'input[name="name"]':    'Asia Wolf',
+			'input[name="email"]':    'asia.nicole.wolf@gmail.com',
+			'#description':   'Testing things whoooo'
+		}, false);
+		this.click('#createProfile');
+		casper.then(function(){
+			test.assertTextExists('asia.nicole.wolf@gmail.com');
+		});
+	}).run(function() {
+		test.done();
+	});
 });
